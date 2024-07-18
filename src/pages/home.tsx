62f4f1dd333396c6f41b6f28d7c07bd737652bd8
Hun <dudgns9194@gmail.com>
2024-07-18 16:53:23 +0900

    기본 컴포넌트 작성 - Button 컴포넌트

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/common/Button";
 import Title from "../components/common/Title";
 
 
@@ -7,6 +8,9 @@ function Home() {
         <Title size='medium' color='background'>
             제목 테스트
         </Title>
+        <Button size='large' scheme='primary'>
+            버튼 테스트
+        </Button>
         <div>home body</div>
         </>
     );
