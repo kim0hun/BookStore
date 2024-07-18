4a236dd061a5b4b68a6d5c5186cfc1c560a0d2f4
Hun <dudgns9194@gmail.com>
2024-07-18 23:07:04 +0900

    헤더와 푸터

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home";
-import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
     <BookStoreThemeProvider>
-      <ThemeSwitcher />
       <Layout>
         <Home />
       </Layout>
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,6 +3,7 @@ export type ThemeName = 'light' | 'dark';
 export type HeadingSize = 'large' | 'medium' | 'small';
 export type ButtonSize = 'large' | 'medium' | 'small';
 export type ButtonScheme = 'primary' | 'normal';
+export type LayoutWidth = 'large' | 'medium' | 'small';
 
 interface Theme {
     name: string;
@@ -27,14 +28,19 @@ interface Theme {
     borderRadius: {
         default: string;
     };
+    layout : {
+        width: {
+            [key in LayoutWidth]: string;
+        };
+    };
 }
 
 export const light: Theme = {
     name: 'light',
     color: {
-        primary: 'brown',
-        background: 'lightgray',
-        secondary: 'blue',
+        primary: '#ff5800',
+        background: 'lightgrey',
+        secondary: '#5F5F5F',
         third: 'green',
         border: 'grey',
         text: 'black'
@@ -76,6 +82,13 @@ export const light: Theme = {
     },
     borderRadius: {
         default: '4px'
+    },
+    layout : {
+        width: {
+            large: '1020px',
+            medium: '760px',
+            small: '320px'
+        }
     }
 };
 
