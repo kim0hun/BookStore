29822f49e5f4ae7c44639e5216ba2bf446cf5bfe
Hun <dudgns9194@gmail.com>
2024-08-02 17:00:19 +0900

    useAuth 훅 만들기

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,12 +1,10 @@
 import Title from "../components/common/Title";
 import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { resetPassword, resetRequest } from "../api/auth.api";
-import { useAlert } from "../hooks/useAlert";
 import { SignupStyle } from "./Signup";
+import { useAuth } from "@/hooks/useAuth";
 
 export interface SignupProps {
   email: string;
@@ -14,10 +12,7 @@ export interface SignupProps {
 }
 
 function ResetPassword() {
-  const navigate = useNavigate();
-  const {showAlert} = useAlert();
-  const [resetRequested, setresetRequested] = useState(false);
-
+  const {userResetPassword, userResetRequest, resetRequested} = useAuth();
   const {
     register,
     handleSubmit,
@@ -25,18 +20,7 @@ function ResetPassword() {
   } = useForm<SignupProps>();
 
   const onSubmit = (data: SignupProps) => {
-    if (resetRequested) {
-      // 초기화
-      resetPassword(data).then(() => {
-        showAlert("비밀번호 초기화되었습니다.");
-        navigate("/login");
-      });
-    } else {
-      // 요청
-      resetRequest(data).then(() => {
-        setresetRequested(true);
-      });
-    }
+    resetRequested ? userResetPassword(data) : userResetRequest(data);
   };
 
   return (
