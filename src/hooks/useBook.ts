7a18964e975327fdc3a70348faf0a6f851666d3c
Hun <dudgns9194@gmail.com>
2024-08-06 23:04:21 +0900

    다양한 UI 경험 - 토스트

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -5,6 +5,7 @@ import { useAuthStore } from "../store/authStore";
 import { useAlert } from "./useAlert";
 import { addCart } from "../api/carts.api";
 import { addBookReview, fetchBookReview } from "@/api/review.api";
+import { useToast } from "./useToast";
 
 export const useBook = (bookId: string | undefined) => {
   const [book, setBook] = useState<BookDetail | null>(null);
@@ -13,6 +14,8 @@ export const useBook = (bookId: string | undefined) => {
 
   const { isloggedIn } = useAuthStore();
   const {showAlert} = useAlert();
+
+  const { showToast } = useToast();
 
   const likeToggle = () => {
     if (!isloggedIn) {
@@ -29,6 +32,7 @@ export const useBook = (bookId: string | undefined) => {
           liked: false,
           likes: book.likes - 1,
         });
+        showToast("좋아요가 취소되었습니다.");
       });
     } else {
       likeBook(book.id).then(() => {
@@ -37,6 +41,7 @@ export const useBook = (bookId: string | undefined) => {
           liked: true,
           likes: book.likes + 1,
         });
+        showToast("좋아요 성공했습니다.");
       });
     }
   };
